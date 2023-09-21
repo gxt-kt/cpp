68c637b830117a95267c41c0afe48da38ecba8da
gxt_kt <gxt_kt.com>
2023-09-21 23:58:19 +0800

    add BST files

---
+++ algorithm/bst/BST.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "common.h"
+
+// TODO: need todo
+// Ref :
+// https://www.geeksforgeeks.org/introduction-to-binary-search-tree-data-structure-and-algorithm-tutorials/?ref=appendix
+
+
---
+++ algorithm/bst/BST_main.cc
@@ -0,0 +1,6 @@
+#include "BST.hpp"
+
+int main (int argc, char *argv[]) {
+  gDebugCol3("need TODO");
+  return 0;
+}
