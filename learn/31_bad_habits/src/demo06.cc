c4d7cef7e57dd680a4d120b3f7b90d03e01358d6
gxt_kt <gxt_kt@163.com>
2023-09-25 10:13:51 +0800

    solve some bugs that compile use gcc low version on ubuntu

--- learn/31_bad_habits/src/demo06.cc
+++ learn/31_bad_habits/src/demo06.cc
@@ -5,7 +5,7 @@ template <typename T>
 void PrintMemoryBytes(const T& input) {
   auto* byte_addr=reinterpret_cast<const std::byte*>(&input); // std::byte need cpp17
   for(int i{0};i<sizeof(T);i++) {
-    std::cout << byte_addr+i << " : " << std::to_integer<int>(*(byte_addr+i)) << "\n";
+    std::cout << byte_addr+i << " : " << static_cast<int>(*(byte_addr+i)) << "\n";
   }
   std::cout << std::endl;
 }
