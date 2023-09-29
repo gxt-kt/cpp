73bb57ff06b6bcdd8001dd0789c147eb98f55c60
gxt_kt <gxt_kt@163.com>
2023-09-29 23:46:55 +0800

    initial complete tuple code

--- std_lib/stl/std_tuple/std_tuple.hpp
+++ std_lib/stl/std_tuple/std_tuple.hpp
@@ -1,3 +1,46 @@
 #pragma once
 #include "common.h"
 
+
+GXT_NAMESPACE_BEGIN
+
+template <typename... Values>
+class tuple;
+
+template <>
+class tuple<> {};
+
+template <typename Head, typename... Tail>
+class tuple<Head, Tail...> : private tuple<Tail...> {
+  typedef tuple<Tail...> inherited;
+
+ public:
+  tuple() {}
+  tuple(Head v, Tail... vtail) : m_head(v), inherited(vtail...) {}
+
+  Head& head() { return m_head; }
+  inherited& tail() { return *this; }
+
+ protected:
+  Head m_head;
+};
+
+template <std::size_t Index, typename Head, typename... Tail>
+struct tuple_get_helper {
+  static auto& get(tuple<Head, Tail...>& t) {
+    return tuple_get_helper<Index - 1, Tail...>::get(t.tail());
+  }
+};
+
+template <typename Head, typename... Tail>
+struct tuple_get_helper<0, Head, Tail...> {
+  static auto& get(tuple<Head, Tail...>& t) { return t.head(); }
+};
+
+template <std::size_t Index, typename... Values>
+auto& get(tuple<Values...>& t) {
+  return tuple_get_helper<Index, Values...>::get(t);
+}
+
+GXT_NAMESPACE_END
+
--- std_lib/stl/std_tuple/std_tuple_main.cc
+++ std_lib/stl/std_tuple/std_tuple_main.cc
@@ -1,5 +1,23 @@
 #include "std_tuple.hpp"
 
+
 int main(int argc, char* argv[]) {
-  gDebugCol3("TODO:") << G_FILE;
+  GXT_NAMESPACE::tuple<int, double, std::string> goo(1, 2.5, "111");
+
+  gDebug() << goo.head();
+  gDebug() << goo.tail().head();
+  gDebug() << goo.tail().tail().head();
+  gDebug() << goo.tail().tail().tail();
+
+  goo.head() = 20;
+  gDebug() << goo.head();
+
+  gDebug(GXT_NAMESPACE::get<0>(goo));
+  gDebug(GXT_NAMESPACE::get<1>(goo));
+  gDebug(GXT_NAMESPACE::get<2>(goo));
+  // gDebug(GXT_NAMESPACE::get<3>(goo)); // error
+
+  gDebug(TYPE(GXT_NAMESPACE::get<0>(goo)));
+  gDebug(TYPE(GXT_NAMESPACE::get<1>(goo)));
+  gDebug(TYPE(GXT_NAMESPACE::get<2>(goo)));
 }
