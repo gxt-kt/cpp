dc632404732c26659a4d01a3a453fcf49cfd5031
gxt_kt <gxt_kt@163.com>
2023-10-09 17:33:27 +0800

    impl std mvoe forward code

--- std_lib/std_move_forward/std_move_forward.hpp
+++ std_lib/std_move_forward/std_move_forward.hpp
@@ -1,10 +1,53 @@
 #pragma once
 #include "common.h"
 
-
 GXT_NAMESPACE_BEGIN
 
+template <typename T>
+struct remove_reference {
+  using type = T;
+};
 
+template <typename T>
+struct remove_reference<T&> {
+  using type = T;
+};
+
+template <typename T>
+struct remove_reference<T&&> {
+  using type = T;
+};
+
+/**
+ *  @brief  Convert a value to an rvalue.
+ *  @param  __t  A thing of arbitrary type.
+ *  @return The parameter cast to an rvalue-reference to allow moving it.
+ */
+template <typename T>
+constexpr typename GXT_NAMESPACE::remove_reference<T>::type&& move(T&& __t) noexcept {
+  return static_cast<typename GXT_NAMESPACE::remove_reference<T>::type&&>(__t);
+}
+
+/**
+ *  @brief  Forward an lvalue.
+ *  @return The parameter cast to the specified type.
+ *
+ *  This function is used to implement "perfect forwarding".
+ */
+template <typename T>
+constexpr T&& forward(typename GXT_NAMESPACE::remove_reference<T>::type& __t) noexcept {
+  return static_cast<T&&>(__t);
+}
+
+/**
+ *  @brief  Forward an rvalue.
+ *  @return The parameter cast to the specified type.
+ *
+ *  This function is used to implement "perfect forwarding".
+ */
+template <typename T>
+constexpr T&& forward(typename GXT_NAMESPACE::remove_reference<T>::type&& __t) noexcept {
+  return static_cast<T&&>(__t);
+}
 
 GXT_NAMESPACE_END
-
--- std_lib/std_move_forward/std_move_forward_main.cc
+++ std_lib/std_move_forward/std_move_forward_main.cc
@@ -1,8 +1,46 @@
 #include "std_move_forward.hpp"
 
-
-int main(int argc, char* argv[]) {
-  gDebug() << G_FILE_LINE;
+template <typename T>
+void FunT(T&& a) {
+  gDebugCol1(TYPET(T));
+  gDebugCol1(TYPE(GXT_NAMESPACE::forward<T>(a)));
 }
 
 
+int main(int argc, char* argv[]) {
+  gDebug() << G_FILE_LINE;
+
+  gDebug() << G_SPLIT_LINE;
+  {
+    int a;
+    FunT(a);
+    FunT(10);
+    FunT(GXT_NAMESPACE::move(a));
+  }
+  gDebug() << G_SPLIT_LINE;
+  {
+    int t;
+    auto&& a = t;
+    auto&& b = 10;
+    auto&& c = GXT_NAMESPACE::move(t);
+    gDebugCol2(TYPE(t));
+    gDebugCol2(TYPE(a));
+    gDebugCol2(TYPE(b));
+    gDebugCol2(TYPE(c));
+  }
+  {
+    auto fun1=[]()->decltype(auto){ int a; return a; };
+    auto fun2=[]()->decltype(auto){ return 10; };
+    auto fun3=[]()->decltype(auto){ int a; return std::move(a); };
+    gDebugCol3(TYPE(fun1()));
+    gDebugCol3(TYPE(fun2()));
+    gDebugCol3(TYPE(fun3()));
+    // add () for return value with decltype(auto)
+    auto fun4=[]()->decltype(auto){ int a; return (a); };
+    auto fun5=[]()->decltype(auto){ return (10); };
+    auto fun6=[]()->decltype(auto){ int a; return (std::move(a)); };
+    gDebugCol3(TYPE(fun4()));
+    gDebugCol3(TYPE(fun5()));
+    gDebugCol3(TYPE(fun6()));
+  }
+}
