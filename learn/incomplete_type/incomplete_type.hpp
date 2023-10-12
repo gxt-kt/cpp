17f76054f2ba87251f4d7aa158f0f38247ef8156
gxt_kt <gxt_kt@163.com>
2023-10-12 11:33:21 +0800

    add incomplete type demo

--- learn/incomplete_type/incomplete_type.hpp
+++ learn/incomplete_type/incomplete_type.hpp
@@ -43,3 +43,38 @@ struct A {
 };
 
 };  // namespace demo03
+
+namespace demo04 {
+struct A {
+  // A a; // error incomplete-types
+};
+struct B {
+  B* a;  // ok
+};
+
+};  // namespace demo04
+
+namespace demo05 {
+struct A {
+  // can use because vector just storage A's address, and sizeof(vector) is
+  // fixed
+  std::vector<A> a;
+
+  // error incomplete-types
+  // std::pair<int, A> b;
+
+  // error incomplete-types
+  // std::variant<A,int> c;
+
+  // only support g++ version > 12
+#if __GNUC__ >= 12
+  std::unordered_map<int, A> d;
+#endif
+
+  // like above only support g++ version > 12
+#if __GNUC__ >= 12
+  std::variant<int, std::unordered_map<int, A>> e;
+#endif
+};
+
+};  // namespace demo05
