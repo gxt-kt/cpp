75b344c94fa276d7b3ce55075c6f2e6ec16213af
gxt_kt <gxt_kt@163.com>
2024-10-29 21:02:06 +0800

    add std [multiple] map/set

---
+++ std_lib/stl/map_set_multiple/std_map_main.cc
@@ -0,0 +1,17 @@
+#include "std_map.hpp"
+
+int main() {
+  std::cout << std::boolalpha;
+  Map<std::string, int> table;
+  table["delay"] = 12;
+  if (!table.contains("delay")) table["delay"] = 32;
+  table["timeout"] = 42;
+
+  for (auto it = table.begin(); it != table.end(); ++it)
+    std::cout << it->first << "=" << it->second << '\n';
+
+  std::cout << "at(delay): " << table.at("delay") << '\n';
+  std::cout << "size: " << table.size() << '\n';
+
+  return 0;
+}
